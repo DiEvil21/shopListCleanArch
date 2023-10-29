3b26392f8a9f74309f8c211d5508328f1beb680e
DiEvil21 <55885322+DiEvil21@users.noreply.github.com>
2023-10-29 19:20:33 +0300

    create packenges for clean arch layers

--- app/src/main/java/ru/dievil/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/ru/dievil/shoplist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.dievil.shoplist
+package ru.dievil.shoplist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.dievil.shoplist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
