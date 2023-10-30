6dc5a1abba70db9d0fdcc5213e388e51ea50341f
DiEvil21 <55885322+DiEvil21@users.noreply.github.com>
2023-10-30 22:09:27 +0300

    create ViewModel in presentation layer

--- app/src/main/java/ru/dievil/shoplist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/ru/dievil/shoplist/data/ShopListRepositoryImpl.kt
@@ -1,23 +1,33 @@
 package ru.dievil.shoplist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import ru.dievil.shoplist.domain.ShopItem
 import ru.dievil.shoplist.domain.ShopListRepository
 import java.lang.RuntimeException
 
 object ShopListRepositoryImpl: ShopListRepository {
-
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
     private val shopList = mutableListOf<ShopItem>()
 
     private var autoIncrementId = 0
+    init {
+        for (i in 0 until 10) {
+            val item = ShopItem("name: $i",i, true)
+            addShopItem(item)
+        }
+    }
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == ShopItem.UNDEFINED_ID) {
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
@@ -32,8 +42,12 @@ object ShopListRepositoryImpl: ShopListRepository {
         } ?: throw RuntimeException("Element with id $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateList() {
+        shopListLD.value = shopList.toList()
     }
 
 
