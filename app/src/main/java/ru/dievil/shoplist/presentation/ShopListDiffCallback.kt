2b50ccfeabc56567ed28b83b062ff554580093b2
DiEvil21 <55885322+DiEvil21@users.noreply.github.com>
2023-10-31 19:17:11 +0300

    recyclerView code, create adapter, ListDiffCallbacks

---
+++ app/src/main/java/ru/dievil/shoplist/presentation/ShopListDiffCallback.kt
@@ -0,0 +1,31 @@
+package ru.dievil.shoplist.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import ru.dievil.shoplist.domain.ShopItem
+
+class ShopListDiffCallback(
+    private val oldlist: List<ShopItem>,
+    private val newList: List<ShopItem>
+):DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldlist.size
+    }
+
+    override fun getNewListSize(): Int {
+        return  newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldlist[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldlist[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        return oldItem == newItem
+    }
+
+
+}
